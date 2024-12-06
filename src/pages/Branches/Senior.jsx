f2631245b7b0dc3db55daee19c94a08086bfff6f
Maria <mariaeduardabenevenutti77@gmail.com>
2024-12-06 08:43:11 -0300

    feat: iniciando desenvolvimento (senior)

--- src/pages/Branches/Senior.jsx
+++ src/pages/Branches/Senior.jsx
@@ -1,4 +1,5 @@
 import HeaderStyled from "../../components/Branches/HeaderStyled";
+import "./styles/Senior.module.css";
 
 export default function Senior() {
   return (
@@ -7,9 +8,12 @@ export default function Senior() {
         <HeaderStyled
           branch={"Senior"}
           description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+            "Pratica de valores como autodomínio, coragem, amizade, respeito e autoconfiança, preparando-os para enfrentarem os desafios com segurança e determinação!"
           }
         />
+        <div>
+          <p>Este ramo reúne jovens com idade entre 15 à 17 anos, de ambos os sexos. As atividades realizadas dentro deste ramo são voltadas para o <strong>desenvolvimento físico</strong>, <strong>moral</strong>, <strong>intelectual</strong>, <strong>espiritual</strong> e por fim o <strong>crescimento social</strong>!</p>
+        </div>
       </div>
     </>
   );
