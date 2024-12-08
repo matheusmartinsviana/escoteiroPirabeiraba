487d3c06a37c8d214dcddcd16f03e441f52655d2
carolainesantos <carolainedossants@gmail.com>
2024-12-08 17:53:27 -0300

    feat: styles

--- src/pages/Branches/Lobinho.jsx
+++ src/pages/Branches/Lobinho.jsx
@@ -1,5 +1,5 @@
 import HeaderStyled from "../../components/Branches/HeaderStyled";
-import styles from "./styles/Senior.module.css";
+import styles from "./styles/Lobinho.module.css";
 import card1 from "../../assets/socializacao.svg"
 import card2 from '../../assets/imaginacao.svg'
 import card3 from "../../assets/respeito.svg"
@@ -44,6 +44,11 @@ export default function Lobinho() {
           Cada matilha tem de quatro a seis lobinhos, entre meninos e meninas, Fazem jogos, brincadeiras, vivem aventuras, 
           aprendem sobre a importância da boa ação diária e ainda são incentivados a fazer sempre o nosso Melhor Possível; 
           esse inclusive é o  lema do ramo lobinho.</p>
+
+          <div>
+          <p className="SubTitleLobo">Tem sua própria lei, a Lei do Lobinho, que traz cinco artigos:</p>
+          </div>
+
           <p className={styles.subText}><strong> -&gt; </strong> A tropa pode ser masculina (Tropa Sênior), feminina (Tropa Guia) ou mista, e é organizada em até quatro patrulhas, cada uma composta por 4 a 6 jovens. Cada patrulha escolhe um nome representativo, inspirado em acidentes geográficos conhecidos ou em tribos indígenas brasileiras.</p>
           <p className={styles.subText}><strong> -&gt; </strong>Na Tropa Sênior/Guia, a relação entre a chefia e os membros se diferencia dos ramos Lobinho e Escoteiro. Aqui, os jovens possuem maturidade suficiente para traçar seus próprios caminhos, recebendo apenas o suporte necessário dos chefes. Esse ramo é conhecido por exigir maior esforço físico e mais conhecimento, proporcionando aos participantes uma evolução significativa.</p>
           <p className={styles.subText}><strong> -&gt; </strong>Antes de completarem 18 anos, os jovens são direcionados ao Clã Pioneiro. Esse momento de transição é marcado pela "ponte", onde começam a se preparar para a nova etapa. A jornada é concluída com uma cerimônia de passagem, na qual se despedem da Tropa Sênior/Guia e celebram essa fase tão especial.</p>
