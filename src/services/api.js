2afe678b222dfd692afc21077b902b0cdcbb8d9c
Matheus Viana <matheusmviana@outlook.com.br>
2025-06-11 08:57:54 -0300

    feat: atualizar package-lock.json com novas dependências e ajustar a configuração da baseURL no axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Criando uma instância do axios com configurações base
 const api = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
